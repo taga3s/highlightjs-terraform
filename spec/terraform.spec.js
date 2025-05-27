1a4ef45c3beb69eaceb3d242def50e9d48a43a8f
taga3s <bepozurapu274@gmail.com>
2025-05-27 21:57:20 +0900

    Move on to ESM and add a  snapshot test

---
+++ spec/terraform.spec.js
@@ -0,0 +1,25 @@
+import hljs from "highlight.js";
+import { definer } from "../terraform";
+import fs from "fs";
+import path from "path";
+import { describe } from "vitest";
+import { it } from "vitest";
+import { expect } from "vitest";
+
+hljs.registerLanguage("terraform", definer);
+
+describe("highlight bundle", () => {
+  it("highlights terraform", () => {
+    const input = fs.readFileSync(
+      path.resolve(__dirname, "./input.txt"),
+      "utf-8"
+    );
+
+    const { value: result, language } = hljs.highlightAuto(input, [
+      "terraform",
+    ]);
+
+    expect(language).toBe("terraform");
+    expect(result).toMatchSnapshot();
+  });
+});
--- terraform.js
+++ terraform.js
@@ -13,75 +13,74 @@
  * Category: scripting
  */
 
-var module = module ? module : {};     // shim for browser use
-
-function hljsDefineTerraform(hljs) {
-	var NUMBERS = {
-		className: 'number',
-		begin: '\\b\\d+(\\.\\d+)?',
-		relevance: 0
-	};
-	var STRINGS = {
-		className: 'string',
-		begin: '"',
-		end: '"',
-		contains: [{
-			className: 'variable',
-			begin: '\\${',
-			end: '\\}',
-			relevance: 9,
-			contains: [{
-				className: 'string',
-				begin: '"',
-				end: '"'
-			}, {
-			className: 'meta',
-			begin: '[A-Za-z_0-9]*' + '\\(',
-			end: '\\)',
-			contains: [
-				NUMBERS, {
-					className: 'string',
-					begin: '"',
-					end: '"',
-					contains: [{
-						className: 'variable',
-						begin: '\\${',
-						end: '\\}',
-						contains: [{
-							className: 'string',
-							begin: '"',
-							end: '"',
-							contains: [{
-								className: 'variable',
-								begin: '\\${',
-								end: '\\}'
-							}]
-						}, {
-							className: 'meta',
-							begin: '[A-Za-z_0-9]*' + '\\(',
-							end: '\\)'
-						}]
-					}]
-          		},
-          	'self']
-			}]
-		}]
-	};
-
-return {
-	aliases: ['tf', 'hcl'],
-	keywords: 'resource variable provider output locals module data terraform|10',
-	literal: 'false true null',
-	contains: [
-   		hljs.COMMENT('\\#', '$'),
-   		NUMBERS,
-		STRINGS
-	]
-}
-}
-
-module.exports = function(hljs) {
-    hljs.registerLanguage('terraform', hljsDefineTerraform);
+const NUMBERS = {
+	className: 'number',
+	begin: '\\b\\d+(\\.\\d+)?',
+	relevance: 0
 };
 
-module.exports.definer = hljsDefineTerraform;
+const STRINGS = {
+	className: 'string',
+	begin: '"',
+	end: '"',
+	contains: [{
+		className: 'variable',
+		begin: '\\${',
+		end: '\\}',
+		relevance: 9,
+		contains: [{
+			className: 'string',
+			begin: '"',
+			end: '"'
+		}, {
+		className: 'meta',
+		begin: '[A-Za-z_0-9]*' + '\\(',
+		end: '\\)',
+		contains: [
+			NUMBERS, {
+				className: 'string',
+				begin: '"',
+				end: '"',
+				contains: [{
+					className: 'variable',
+					begin: '\\${',
+					end: '\\}',
+					contains: [{
+						className: 'string',
+						begin: '"',
+						end: '"',
+						contains: [{
+							className: 'variable',
+							begin: '\\${',
+							end: '\\}'
+						}]
+					}, {
+						className: 'meta',
+						begin: '[A-Za-z_0-9]*' + '\\(',
+						end: '\\)'
+					}]
+				}]
+						},
+					'self']
+		}]
+	}]
+};
+
+function hljsDefineTerraform(hljs) {
+	return {
+		aliases: ['tf', 'hcl'],
+		keywords: 'resource variable provider output locals module data terraform|10',
+		literal: 'false true null',
+		contains: [
+			hljs.COMMENT('\\#', '$'),
+			NUMBERS,
+			STRINGS
+		]
+	}
+}
+
+export default function(hljs) {
+	hljs.registerLanguage('terraform', hljsDefineTerraform);
+}
+
+export { hljsDefineTerraform as definer };
