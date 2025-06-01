a49c0987f6bcf902c4514999148c0d4712bce57f
taga3s <bepozurapu274@gmail.com>
2025-06-01 14:22:45 +0900

    Add publish.yml

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { HLJSApi, LanguageFn } from "highlight.js";
-declare const hljsDefineTerraform: LanguageFn;
-export default function (hljs: HLJSApi): void;
-export { hljsDefineTerraform as definer };
